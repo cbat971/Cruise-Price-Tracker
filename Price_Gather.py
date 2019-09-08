b20e32a278657636a67cfbce36aa027f7bee613d
cbat971 <40727936+cbat971@users.noreply.github.com>
2019-09-08 14:52:49 -0400

    Create Price_Gather.py
    #This is mostly to learn how to use git as well as learn some new technologies like...
      #web scraping
      #data management
      #sending alerts in python

---
+++ Price_Gather.py
@@ -0,0 +1,10 @@
+#program will check on prices of cruises out of a given zip code to find deals and discounts
+# a goal of this code is to log cruise prices and send an alert when these prices drop as well as maybe
+#predict the next price drop accordingly
+
+#This is mostly to learn how to use git as well as learn some new technologies like...
+  #web scraping
+  #data management
+  #sending alerts in python
+  
+  
